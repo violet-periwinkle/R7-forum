61fa6528a7c35300298ffc8f3e339f6816b86b78
violet-periwinkle <134008799+violet-periwinkle@users.noreply.github.com>
2024-07-27 11:58:50 -0400

    lesson 6 (second time through)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  #resources :users
+  post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
+  delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  post '/users', to: 'users#create'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#delete'
+  resources :forums
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

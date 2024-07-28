a01ba00f13c5c33b1e0e48f7f4d308d660bed21f
violet-periwinkle <134008799+violet-periwinkle@users.noreply.github.com>
2024-07-28 15:04:40 -0400

    step 3: creating routes and the post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
+  get 'posts/create'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
   #resources :users
   root 'forums#index'
   post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
-  delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
   get '/users', to: 'users#index', as: 'users'
   get '/users/new', to: 'users#new', as: 'new_user'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   post '/users', to: 'users#create'
   patch '/users/:id', to: 'users#update'
+  delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
   delete '/users/:id', to: 'users#delete'
-  resources :forums
+  resources :forums do
+    resources :posts, shallow: true, except: [:index]
+    resources :subscriptions, shallow: true, except: [:index]
+  end
+  get '/subscriptions', to: 'subscriptions#index', as: 'subscriptions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

eb5eff02205dc1276588faf42acfbe02e83d2a1b
violet-periwinkle <134008799+violet-periwinkle@users.noreply.github.com>
2024-07-28 13:25:49 -0400

    step 2: creating the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-    validates :skill_level, inclusion: { in: %w(beginner intermediate expert) }
+  validates :skill_level, inclusion: { in: %w(beginner intermediate expert) }
+  has_many :subscriptions
+  has_many :posts
+  has_many :forums, through: :subscriptions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_23_220124) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_28_171455) do
   create_table "forums", force: :cascade do |t|
     t.string "forum_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "forum_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["forum_id"], name: "index_posts_on_forum_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "forum_id", null: false
+    t.integer "user_id", null: false
+    t.integer "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["forum_id"], name: "index_subscriptions_on_forum_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_23_220124) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "forums"
+  add_foreign_key "posts", "users"
+  add_foreign_key "subscriptions", "forums"
+  add_foreign_key "subscriptions", "users"
 end
